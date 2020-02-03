f95fd82e74fa1c8a455891f3506a46b8ac879ce5
auxaneN <a.nitharum@sfr.fr>
2020-02-03 03:19:30 -0500

    Updates

---
+++ precache-manifest.71a16f359efef8d4341b92a1d64d6b16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b76f7f0ca1edca80ec2bcab72f0aeebe",
+    "url": "/monsterrolodex-react/index.html"
+  },
+  {
+    "revision": "d9698a7b7128a673b460",
+    "url": "/monsterrolodex-react/static/css/main.d3732960.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsterrolodex-react/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsterrolodex-react/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9698a7b7128a673b460",
+    "url": "/monsterrolodex-react/static/js/main.b58c78c0.chunk.js"
+  },
+  {
+    "revision": "23461c3f6f680f42cf0b",
+    "url": "/monsterrolodex-react/static/js/runtime-main.927bdcae.js"
+  }
+]);
